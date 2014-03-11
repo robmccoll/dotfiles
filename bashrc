85f79c5a038b97a123f2f261ee83962cd1a9b5ba
rob@robmccoll.com <vagrant@vagrant.vm>
2014-03-11 03:51:16 +0000

    adding bash prompt, screen config

---
+++ bashrc
@@ -0,0 +1,12 @@
+# Source global definitions
+if [ -f /etc/bashrc ]; then
+	. /etc/bashrc
+fi
+
+# User specific aliases and functions
+
+PS1="\[\033[01;34m\][\[\033[01;31m\]\u@\h\[\033[01;34m\]] [\[\033[01;31m\]\t\[\033[01;34m\]] [\[\033[01;31m\]\w\[\033[01;34m\]]
+$\[\033[00m\]"
+
+export SVN_EDITOR=vim
+export EDITOR=vim
---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+add_to_home() {
+  if [ -f ~/.$1 ]; then
+    mv -n ~/.$1 ~/.old_dotfiles
+  fi
+  if [ ! -f ~/.$1 ]; then
+    ln $1 ~/.$1
+  fi
+}
+
+add_dir_to_home() {
+  if [ -d ~/.$1 ]; then
+    mv -n ~/.$1 ~/.old_dotfiles
+  fi
+  if [ ! -d ~/.$1 ]; then
+    if [ ! -L ~/.$1 ]; then
+      ln -s $PWD/$1 ~/.$1
+    fi
+  fi
+}
+
+
+mkdir -p ~/.old_dotfiles
+
+add_to_home "bashrc"
+add_to_home "vimrc"
+add_dir_to_home "vim"
+add_to_home "screenrc"
+
+. ~/.bashrc
